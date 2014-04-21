be2379af5378b48d325827fc0993d4b87f5c3c47
chetandhembre <chetandhembre@gmail.com>
2014-04-21 09:27:26 +0530

    added test case

--- test/test.js
+++ test/test.js
@@ -6,36 +6,179 @@
 
 'use strict';
 
-var lRequire =  require('../');
+
 var test =  require('tap').test;
 
-test('1st argument should be string', function (t) {
 
-    lRequire.register({}, {}, function (err, response) {
-        t.equal(err.message, 'namespace should be string', '1st argument should be string');
-        t.end();
+
+
+/*
+* thing to test
+* 1) sync api and async api
+* following things should be tested properly
+* i) argument should be in proper format
+* ii)
+*
+*
+* */
+
+test('test async api', function (t) {
+  t.plan(8);
+  var lRequire =  require('../');
+  t.ok(lRequire, 'local-require module loaded successfully');
+  t.test('1st argument should be object', function (t1) {
+    lRequire.register('demo', function (err, response) {
+      t1.equal(err.message, '1st argument should be object');
+      t1.end();
     });
+  });
+
+  t.test('namespace should be present', function (t) {
+    lRequire.register({}, function (err, response) {
+      t.equal(err.message, 'namespace should be string');
+      t.end();
+    });
+  });
+
+  t.test('namespace should be string', function (t) {
+    lRequire.register({
+      namespace : {}
+    }, function (err, response) {
+      t.equal(err.message, 'namespace should be string');
+      t.end();
+    });
+  });
+
+  t.test('config should be present', function (t) {
+    lRequire.register({
+      namespace : 'demo'
+    }, function (err, response) {
+      t.equal(err.message, 'configuration should be an object');
+      t.end();
+    });
+  });
+
+  t.test('config should be object', function (t) {
+    lRequire.register({
+      namespace : 'demo',
+      config : 'demo'
+    }, function (err, response) {
+      t.equal(err.message, 'configuration should be an object');
+      t.end();
+    });
+  });
+
+  t.test('node_modules folder is not found', function (t) {
+    var home = process.cwd();
+    process.chdir('/home');
+    lRequire.register({
+      namespace : 'demo',
+      config : {}
+    }, function (err, response) {
+      t.equal(err.message, 'unable to find node_modules folder');
+      t.end();
+      process.chdir(home);
+    });
+  });
+
+  t.test('node_modules folder is not found', function (t) {
+    lRequire.register({
+      namespace : 'demo',
+      config : {
+        '1' : '../1.js'
+      }
+    }, function (err, response) {
+      t.equal(err.message.indexOf('NOT Found'), 0);
+      t.end();
+    });
+  });
 });
 
-test('configuration should be object', function (t) {
-    lRequire.register('demo', 'demo', function (err, response) {
-        t.equal(err.message, 'configuration should be an object', 'configuration should be an object');
-        t.end();
-    });
+
+test('test sync api', function (t) {
+  t.plan(8);
+  var lRequire =  require('../');
+  t.ok(lRequire, 'local-require module loaded successfully');
+  t.test('1st argument should be object', function (t1) {
+    try {
+      lRequire.registerSync('demo');
+    } catch (err) {
+      t1.equal(err.message, '1st argument should be object');
+    }
+    t1.end();
+  });
+
+  t.test('namespace should be present', function (t) {
+    try {
+      lRequire.registerSync({});
+    } catch (err) {
+      t.equal(err.message, 'namespace should be string');
+      t.end();
+    }
+  });
+
+  t.test('namespace should be string', function (t) {
+    try {
+      lRequire.registerSync({
+        namespace : {}
+      });
+    } catch (err) {
+      t.equal(err.message, 'namespace should be string');
+      t.end();
+    }
+  });
+
+  t.test('config should be present', function (t) {
+    try {
+      lRequire.registerSync({
+        namespace : 'demo'
+      });
+    } catch (err) {
+      t.equal(err.message, 'configuration should be an object');
+      t.end();
+    }
+  });
+
+  t.test('config should be object', function (t) {
+    try {
+      lRequire.registerSync({
+        namespace : 'demo',
+        config : 'demo'
+      });
+    } catch (err) {
+      t.equal(err.message, 'configuration should be an object');
+      t.end();
+    }
+  });
+
+  t.test('node_modules folder is not found', function (t) {
+    var home = process.cwd();
+    process.chdir('/home');
+    try {
+      lRequire.registerSync({
+        namespace : 'demo',
+        config : {}
+      });
+    } catch (err) {
+      t.equal(err.message, 'unable to find node_modules folder');
+      t.end();
+    }
+    process.chdir(home);
+  });
+
+  t.test('src of file is not found', function (t) {
+    try {
+      lRequire.registerSync({
+        namespace : 'demo',
+        config : {
+          '1' : '../1.js'
+        }
+      });
+    } catch (err) {
+      t.equal(err.message.indexOf('ENOENT, no such file or directory'), 0);
+      t.end();
+    }
+
+  });
 });
 
-test('namespace is already present', function (t) {
-    lRequire.register('tap', {}, function (err, response) {
-        t.equal(err.message, 'namespace already present', 'namespace already present');
-        t.end();
-    });
-});
-//
-//test('config file path is not specified', function (t) {
-//    lRequire.register('demo', {
-//        "1" : "../node_modules/1.js"
-//    }, function (err, response) {
-//        t.equal(err.message, 'namespace already present', 'namespace already present');
-//        t.end();
-//    });
-//});
